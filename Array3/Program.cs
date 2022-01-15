5be9a8ea331a48ccebd9dc621ce9bc0bf366b89d
Pablo Luiz <80119424+devpabloluiz@users.noreply.github.com>
2022-01-15 15:13:56 -0300

    Add files via upload

---
+++ Array3/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+
+// Alterando nome de uma lista (arrays)
+
+public class Program
+{
+    static void AlterarNome(string[] nomes, string nome, string nomeNovo)
+    {   
+        for(int i = 0; i < nomes.Length; i++)
+        {
+            if(nomes[i] == nome)
+            {
+                nomes[i] = nomeNovo;
+            }
+        }
+    }
+    static void Main()
+    {
+        var nomes = new string[]{"Pedro", "José", "Maria", "Júlio", "Pablo"};
+        WriteLine($@"A lista de nomes é:
+            {string.Join(", \n", nomes)}
+            ");
+
+        WriteLine("Qual nome você gosta de trocar?");
+        var nome = ReadLine();
+        WriteLine("Digite o nome novo:");
+        var nomeNovo = ReadLine();
+
+        AlterarNome(nomes, nome, nomeNovo);
+
+        WriteLine($@"A lista de nomes alterada é:
+            {string.Join(", \n", nomes)}
+            ");
+    }
+}
