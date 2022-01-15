46b12ec66c6bc0ee846f1d07645c3c5faf00b6ef
Pablo Luiz <80119424+devpabloluiz@users.noreply.github.com>
2022-01-15 11:51:31 -0300

    Add files via upload

---
+++ Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+public class Program
+{
+    static void MudarParaImpar(int[] pares)
+    {
+        for(int i = 0; i < pares.Length; i++ )
+        {
+            pares[i] = pares[i] + 1;
+        }
+    }
+
+    public static void Main()
+    {
+        int[] pares = new int[]{0,2,4,6,8};
+
+        MudarParaImpar(pares);
+
+        WriteLine($"Os ímpares {string.Join(",", pares)}");
+    }
+}
