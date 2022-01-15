f72dd19579ed0618fe2c48d40c80f6be02c93795
Pablo Luiz <80119424+devpabloluiz@users.noreply.github.com>
2022-01-15 12:27:05 -0300

    Add files via upload

---
+++ Array2/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+
+// Criar uma Console application para encontrar um número inteiro em um array;
+
+public class Program
+{
+    static int EncontrarNumero(int[] numeros, int numero)
+    {
+        for(int i = 0; i < numeros.Length; i++)
+        {
+            if (numeros[i] == numero)
+             return i;
+        }
+             return -1;
+    }
+    public static void Main()
+    {
+        int[] numeros = new int[] {0,2,4,6,8};
+        WriteLine($"Digite o número que gostaria de encontrar");
+        var numero = int.Parse(ReadLine());
+        var idxEncontrado = EncontrarNumero(numeros, numero);
+
+        if(idxEncontrado >= 0)
+        {
+            WriteLine($"O número digitado está na posição {idxEncontrado}");
+        }
+        else
+        {
+            WriteLine("O número digitado não foi encontrado!");
+        }        
+    }
+}
